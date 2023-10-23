399cd986c75ef89d5444bd5620e0380265904c21
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2023-10-23 17:34:53 +1100

    Switched to Git submodules

--- prepare.sh
+++ prepare.sh
@@ -1,26 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ -d kernel ]]; then
-  # kernel source already exists, update it
-  echo "Updating kernel source..."
-  echo "NOTE: This will discard ALL changes!\nThe current version of the kernel can be found at kernel-old/"
-
-  # save a backup
-  rm -rf kernel-old/
-  mkdir kernel-old
-  cp -R kernel/* kernel-old/
-
-  # synchronize source
-  cd kernel
-  git fetch origin
-  git reset --hard origin/main
-  git clean -f -d
-  make clean # for safe measure
-  cd ..
-else
-  # clone kernel source
-  echo "Kernel source not found, cloning..."
-  git clone https://github.com/itsmevjnk/sysx.git kernel
-fi
+git submodule init
+git submodule update
 
 echo "Source preparation complete! Run ./configure.sh <target> to configure the build target."
